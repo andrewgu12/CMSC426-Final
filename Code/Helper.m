cf5bc9cc18a8024e8a48b2b482856a4e645aee2f
Ming Ye <mingye@wireless-10-104-174-19.umd.edu>
2018-05-08 01:03:05 -0400

    RansacDone

--- Code/Helper.m
+++ Code/Helper.m
@@ -37,26 +37,6 @@ ID = imread([Path,'scene_',SceneName,'/frames/image_',FrameNum,'_depth.png']);
 [pcx, pcy, pcz, r, g, b, D_, X, Y,validInd] = depthToCloud_full_RGB(ID, I, './params/calib_xtion.mat');
 Pts = [pcx pcy pcz];
 
-%%
-
-%  
-% imageSize = size(I);
-% [xx,yy] = ndgrid((1:imageSize(1))-yo,(1:imageSize(2))-xo);
-% mask = uint8((xx.^2 + yy.^2) < r^2);
-% cropped = uint8(zeros(size(I)));
-% cropped(:,:,1) = I(:,:,1).*mask;
-% cropped(:,:,2) = I(:,:,2).*mask;
-% cropped(:,:,3) = I(:,:,3).*mask;
-% % I = cropped(yo-r:yo+r,xo-r:xo+r);
-% 
-% imageSize = size(ID);
-% [xx,yy] = ndgrid((1:imageSize(1))-yo,(1:imageSize(2))-xo);
-% mask = uint16((xx.^2 + yy.^2) < r^2);
-% cropped = ID.*mask;
-% % ID = cropped(yo-r:yo+r,xo-r:xo+r);
-% 
-% imshow(I);
-
 %% Display Images and 3D Points
 % Note this needs the computer vision toolbox.
 figure,
@@ -75,31 +55,42 @@ close all;
 
 %% Use Ransac to detect a plane in the scene
 %i = 1;
-k = 250;
-ro = 150;
-xo = 320;
-yo = 240;
+k = 300;
+ro = 500;
+xo = 0;
+yo = 0;
+zo = 0;
+
+%dist = sqrt(sum((center - Pts(b)) .^ 2));
 
 % Initialize final size of inlier
 max = [];
-RGB = [r g b];
-% 
-% pts = ((Pts(:,1)-yo).^2 + (Pts(:,2)-xo).^2) < r.^2;
-% 
-% shrunkPts = find(pts>0);
-% subsetPcx = pcx(shrunkPts,:);
-% subsetPcy = pcy(shrunkPts,:);
-% subsetPcz = pcz(shrunkPts,:);
-% subsetR = r(shrunkPts,:);
-% subsetG = g(shrunkPts,:);
-% subsetB = b(shrunkPts,:);
-% Pts = [subsetPcx subsetPcy subsetPcz];
-% RGB = [subsetR subsetG subsetB];
 
+% 
+pts = ((Pts(:,1)-yo).^2 + (Pts(:,2)-xo).^2 + (Pts(:,2)-zo).^2) < ro.^2;
+
+shrunkPts = find(pts>0);
+subsetPcx = pcx(shrunkPts,:);
+subsetPcy = pcy(shrunkPts,:);
+subsetPcz = pcz(shrunkPts,:);
+subsetR = r(shrunkPts,:);
+subsetG = g(shrunkPts,:);
+subsetB = b(shrunkPts,:);
+Pts = [subsetPcx subsetPcy subsetPcz];
+RGB = [subsetR subsetG subsetB];
+
+figure,
+pcshow(Pts,RGB/255);
+drawnow;
+title('3D Point Cloud');
+
+%%
 for i = 1:k        
      % Set up inlier
-     inliers = [];
-     inlierRGB = [];
+     index = 1;
+     inliers = zeros(length(Pts),3);
+     inliersInd = zeros(length(Pts),1);
+     inlierRGB = zeros(length(Pts),3);
      
      % Pick three points
      sample_pts = randperm(length(Pts),3);
@@ -111,28 +102,51 @@ for i = 1:k
      normal = cross(Pt1-Pt2, Pt1-Pt3);
      syms x y z;
      Pt = [x,y,z];
-     planefunction = dot(normal, Pt-Pt1); 
-     % a,b,c,d
-     coefficients = double(fliplr(coeffs(planefunction,[x y z])));
+     %planefunction = dot(normal, Pt-Pt1); 
      
-     % build out and calculate entire distances matrix
-     modifiedPts = [Pts ones(size(Pts,1),1)]';
-     denominator = sqrt(coefficients(1)^2 + coefficients(2)^2 + coefficients(3)^2);
-     distancesMatrix = (coefficients * modifiedPts) ./ denominator;
+     normalUnit = normal/norm(normal);
+     
+     for j = 1:length(Pts)
+         dist2Pts = (Pt1-Pts(j,:))./norm(Pt1-Pts(j,:));
+         dist = dot(dist2Pts, normalUnit);
+         % find the points within a certain threshold
+         if abs(dist) < .07
+             inliersInd(index,1) = j;
+             inliers(index,1) = Pts(j,1);
+             inliers(index,2) = Pts(j,2);
+             inliers(index,3) = Pts(j,3);
+             inlierRGB(index,1) = RGB(j,1);
+             inlierRGB(index,2) = RGB(j,2);
+             inlierRGB(index,3) = RGB(j,3);
+             index = index+1;
+         end
+     end
+     
+%      % a,b,c,d
+%      coefficients = double(fliplr(coeffs(planefunction,[x y z])));
+%      
+%      % build out and calculate entire distances matrix
+%      modifiedPts = [Pts ones(size(Pts,1),1)]';
+%      denominator = sqrt(coefficients(1)^2 + coefficients(2)^2 + coefficients(3)^2);
+%      distancesMatrix = (coefficients * modifiedPts) ./ denominator;
      
      % find the points within a certain threshold
-     inlierIndices = find(abs(distancesMatrix) < 40);
-     inliers(:,1) = Pts(inlierIndices,1);
-     inliers(:,2) = Pts(inlierIndices,2);
-     inliers(:,3) = Pts(inlierIndices,3);
-     inlierRGB(:,1) = RGB(inlierIndices,1);
-     inlierRGB(:,2) = RGB(inlierIndices,2);
-     inlierRGB(:,3) = RGB(inlierIndices,3);
+%      inlierIndices = find(abs(distancesMatrix) < 40);
+%      inliers(:,1) = Pts(inlierIndices,1);
+%      inliers(:,2) = Pts(inlierIndices,2);
+%      inliers(:,3) = Pts(inlierIndices,3);
+%      inlierRGB(:,1) = RGB(inlierIndices,1);
+%      inlierRGB(:,2) = RGB(inlierIndices,2);
+%      inlierRGB(:,3) = RGB(inlierIndices,3);
+
+     inliers = inliers(1:index-1,:);
+     inlierRGB = RGB(1:index-1,:);
+     inliersInd = inliersInd(1:index-1,:);
      
      if (length(inliers) > length(max))
          max = inliers;
      end
-     %i = i+1;
+
 end
 
 
@@ -142,4 +156,40 @@ figure,
 pcshow(inliers,inlierRGB/255);
 drawnow;
 title('3D Point Cloud');
-     
+
+%%
+
+Pts(inliersInd,:) = [];
+
+RGB(inliersInd,:) = [];
+
+%%
+
+figure,
+pcshow(Pts,RGB/255);
+drawnow;
+title('3D Point Cloud');
+
+
+%%
+
+meanPts = mean(Pts);
+
+
+%  
+% imageSize = size(I);
+% [xx,yy] = ndgrid((1:imageSize(1))-yo,(1:imageSize(2))-xo);
+% mask = uint8((xx.^2 + yy.^2) < r^2);
+% cropped = uint8(zeros(size(I)));
+% cropped(:,:,1) = I(:,:,1).*mask;
+% cropped(:,:,2) = I(:,:,2).*mask;
+% cropped(:,:,3) = I(:,:,3).*mask;
+% % I = cropped(yo-r:yo+r,xo-r:xo+r);
+% 
+% imageSize = size(ID);
+% [xx,yy] = ndgrid((1:imageSize(1))-yo,(1:imageSize(2))-xo);
+% mask = uint16((xx.^2 + yy.^2) < r^2);
+% cropped = ID.*mask;
+% % ID = cropped(yo-r:yo+r,xo-r:xo+r);
+% 
+% imshow(I);
