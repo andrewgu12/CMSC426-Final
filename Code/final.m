bf77288cf83a4861a78db8fbca0a281e631607b2
cchen117 <hchen117@terpmail.umd.edu>
2018-05-10 13:58:50 -0400

    Update final.m

--- Code/final.m
+++ Code/final.m
@@ -23,6 +23,7 @@ function singleObjects = single_object_count(singleRootDirNames, sceneImage, sce
             dirPath = strcat(dirPath, 'frame_');
         end
         rgbImage = rgb2gray(imcrop(imread(strcat(dirPath, '0_rgb.png')), [0,0,450,600]));
+        rgbImage = rgbImage(100:350,150:400);
         singlePoints = detectSURFFeatures(rgbImage);
         [singleFeatures, singlePoints] = extractFeatures(rgbImage, singlePoints);
         pairs = matchFeatures(singleFeatures, sceneFeatures);
@@ -30,6 +31,28 @@ function singleObjects = single_object_count(singleRootDirNames, sceneImage, sce
         matchedObjectPoints = singlePoints(pairs(:,1),:);
         figure;
         showMatchedFeatures(rgbImage, sceneImage, matchedObjectPoints, matchedScenePoints, 'montage');
+        
+        matchedObjectCount = length(matchedObjectPoints.Location);
+        matchedSceneCount = length(matchedScenePoints.Location);
+        
+        if matchedObjectCount >= 3 && matchedSceneCount >= 3
+            [tform, inlierBoxPoints, inlierScenePoints] = estimateGeometricTransform(matchedObjectPoints,matchedScenePoints,'affine');
+        
+            boxPolygon = [1, 1;...                   % top-left
+            size(rgbImage, 2), 1;...                 % top-right
+            size(rgbImage, 2), size(rgbImage, 1);... % bottom-right
+            1, size(rgbImage, 1);...                 % bottom-left
+            1, 1];                   % top-left again to close the polygon
+
+            newBoxPolygon = transformPointsForward(tform, boxPolygon);
+            
+            figure;
+            imshow(sceneImage);
+            hold on;
+            line(newBoxPolygon(:, 1), newBoxPolygon(:, 2), 'Color', 'y');
+            title('Detected Box');
+        end
+        
         if length(pairs) ~= 0
             singleObjects(end+1) = imageCounter;
         end
